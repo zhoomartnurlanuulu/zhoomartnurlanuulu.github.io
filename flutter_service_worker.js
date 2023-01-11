4e739c8a45e5b167194226cd2d094051458957dc
Zhoomart Nurlan Uulu <78846614+zhoomartnurlanuulu@users.noreply.github.com>
2023-01-11 16:17:42 +0600

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3db4d3558c0a11dc4942934a8f4524b7",
-"index.html": "e8d762403a860498d129caddd0c77c76",
-"/": "e8d762403a860498d129caddd0c77c76",
-"main.dart.js": "766536a16774a1d72c3e023b545dff44",
+"index.html": "287eb4420e6d668329ef021a316a3e4c",
+"/": "287eb4420e6d668329ef021a316a3e4c",
+"main.dart.js": "eb018876d87daa5205215ca3c341e493",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
